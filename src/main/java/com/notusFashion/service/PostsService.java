47b3d5ec80425510bc1a3f23881316c530d8eda4
dofany <kimdohwan17@gmail.com>
2020-10-05 15:52:37 +0900

    게시판 업데이트 기능 추가

--- src/main/java/com/notusFashion/service/PostsService.java
+++ src/main/java/com/notusFashion/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.notusFashion.service;
 
+import com.notusFashion.domain.posts.Posts;
 import com.notusFashion.domain.posts.PostsRepository;
+import com.notusFashion.web.dto.PostsResponseDto;
 import com.notusFashion.web.dto.PostsSaveRequestDto;
+import com.notusFashion.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,8 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
     }
 }
